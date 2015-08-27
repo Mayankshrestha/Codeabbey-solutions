9b94f2f66e41ab488080f2727beb43c532c1f106
Mayank Shrestha <makshrestha@gmail.com>
2015-08-27 09:16:22 +0545

    problem_3.py

--- problem_2.py
+++ problem_2.py
@@ -1,8 +1,8 @@
 x = 8
 sum_list = [10, 20, 30, 40, 5, 6, 7, 8]
 n = 0
-while n < x:
-	r = sum_list[0:n+1]
+while n <= x:
+	r = sum_list[0:n]
 	n = n +1
 	result = sum(r)
 print result
--- problem_3.py
+++ problem_3.py
@@ -1,8 +1,10 @@
 arr1 = [100, 15, 1954]
 arr2 = [8, 245, 54]
+result = []
 x = len(arr1)
 i = 0
 while i < x:
-	result = arr1[i] + arr2[i]
+	sum = arr1[i] + arr2[i]
+	result.append (sum)
 	i += 1
 print result
